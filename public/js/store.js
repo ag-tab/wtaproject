66e697bf84d97cd9bcc07dcfc9bed30b35643642
shalakasankar <shalakasankar@gmail.com>
2022-05-08 14:30:13 +0530

    Products page completed

--- public/js/store.js
+++ public/js/store.js
@@ -37,9 +37,9 @@ function showSlides(n) {
         else if(document.getElementById("70").style.background=="rgba(155, 153, 153, 0.56)"){
         amt= e*75000;}
 
-        else {amt=0;
-            alert(document.getElementById("60").style.background);
-        }
+        // else {amt=0;
+        //     alert(document.getElementById("60").style.background);
+        // }
 
        amt= "Rs "+amt+"/-";
 
@@ -69,4 +69,120 @@ function showSlides(n) {
     dropdown.selectedIndex=0;
     document.getElementById("amt").innerHTML = "";
 
+}
+
+function Size1(){
+  document.getElementById("1").style.background= "rgba(155, 153, 153, 0.562)";
+  // document.getElementById("60").style.background = "#d1b94c";
+  document.getElementById("2").style.background = "white";
+  document.getElementById("3").style.background = "white";
+  document.getElementById("pricelock").innerHTML = "Rs:500";
+  document.getElementById("namelock").innerHTML= "Locks for Carbon Telescopic Poles (size1)"
+  var dropdown= document.getElementById("quantitylock");
+  dropdown.selectedIndex=0;
+  document.getElementById("amtlock").innerHTML = "";
+
+}
+
+function Size2(){
+  document.getElementById("2").style.background= "rgba(155, 153, 153, 0.562)";
+  // document.getElementById("60").style.background = "#d1b94c";
+  document.getElementById("1").style.background = "white";
+  document.getElementById("3").style.background = "white";
+  document.getElementById("Pack").style.background = "white";
+  document.getElementById("pricelock").innerHTML = "Rs:600";
+  document.getElementById("namelock").innerHTML= "Locks for Carbon Telescopic Poles (size2)"
+  var dropdown= document.getElementById("quantitylock");
+  dropdown.selectedIndex=0;
+  document.getElementById("amtlock").innerHTML = "";
+
+}
+function Size3(){
+  document.getElementById("3").style.background= "rgba(155, 153, 153, 0.562)";
+  // document.getElementById("60").style.background = "#d1b94c";
+  document.getElementById("1").style.background = "white";
+  document.getElementById("2").style.background = "white";
+  document.getElementById("Pack").style.background = "white";
+  document.getElementById("pricelock").innerHTML = "Rs:700";
+  document.getElementById("namelock").innerHTML= "Locks for Carbon Telescopic Poles (size3)"
+  var dropdown= document.getElementById("quantitylock");
+  dropdown.selectedIndex=0;
+  document.getElementById("amtlock").innerHTML = "";
+
+
+}
+function Pack(){
+  document.getElementById("Pack").style.background= "rgba(155, 153, 153, 0.562)";
+  // document.getElementById("60").style.background = "#d1b94c";
+  document.getElementById("1").style.background = "white";
+  document.getElementById("2").style.background = "white";
+  document.getElementById("3").style.background = "white";
+  document.getElementById("pricelock").innerHTML = "Rs:1,750";
+  document.getElementById("namelock").innerHTML= "Locks for Carbon Telescopic Poles(Pack)"
+  var dropdown= document.getElementById("quantitylock");
+  dropdown.selectedIndex=0;
+  document.getElementById("amtlock").innerHTML = "";
+
+}
+
+function FourBelt(){
+  document.getElementById("4").style.background= "rgba(155, 153, 153, 0.562)";
+  // document.getElementById("70").style.background = "#d1b94c";
+  document.getElementById("6").style.background = "white";
+  document.getElementById("pricemachine").innerHTML = "Rs:1,50,000";
+  document.getElementById("namemachine").innerHTML= "Dehusking Machine(4 belt)"
+ var dropdown= document.getElementById("quantitymachine");
+ dropdown.selectedIndex=0;
+ document.getElementById("amtmachine").innerHTML = "";
+
+
+}
+
+function SixBelt(){
+  document.getElementById("6").style.background= "rgba(155, 153, 153, 0.562)";
+  // document.getElementById("60").style.background = "#d1b94c";
+  document.getElementById("4").style.background = "white";
+  document.getElementById("pricemachine").innerHTML = "Rs: 2,25,000";
+  document.getElementById("namemachine").innerHTML= "Dehusking Machine(6 belt)"
+  var dropdown= document.getElementById("quantitymachine");
+  dropdown.selectedIndex=0;
+  document.getElementById("amtmachine").innerHTML = "";
+
+}
+
+function CalculateOutputLock(){
+   amt="";
+  var e = document.getElementById("quantitylock").value;
+
+  if(document.getElementById("1").style.background=="rgba(155, 153, 153, 0.56)"){
+    amt= e*500;}
+    else if(document.getElementById("2").style.background=="rgba(155, 153, 153, 0.56)"){
+    amt= e*600;}
+    else if(document.getElementById("3").style.background=="rgba(155, 153, 153, 0.56)"){
+      amt= e*700;}
+      else if(document.getElementById("Pack").style.background=="rgba(155, 153, 153, 0.56)"){
+        amt= e*1700;}
+       
+ amt= "Rs "+amt+"/-";
+
+document.getElementById("amtlock").innerHTML = amt;
+}
+
+
+function CalculateOutputmachine(){
+
+  var e = document.getElementById("quantitymachine").value;
+
+  if(document.getElementById("4").style.background=="rgba(155, 153, 153, 0.56)"){
+  amt= e*150000;}
+  else if(document.getElementById("6").style.background=="rgba(155, 153, 153, 0.56)"){
+  amt= e*225000;}
+
+  // else {amt=0;
+  //     alert(document.getElementById("60").style.background);
+  // }
+
+ amt= "Rs "+amt+"/-";
+
+document.getElementById("amtmachine").innerHTML = amt;
 }
